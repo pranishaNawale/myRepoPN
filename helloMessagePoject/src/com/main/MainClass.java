835848a7760629409cfc92560053b20f541c76e9
ADMIN <ADMIN@DESKTOP-6B9O9KH>
2020-06-02 10:39:23 -1200

    added by  pranisha

---
+++ helloMessagePoject/src/com/main/MainClass.java
@@ -0,0 +1,11 @@
+package com.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("Hello ,Pranisha Here");
+	}
+
+}
