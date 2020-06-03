45666dae1cf8052949b7d8618b6b34b759c411b7
ADMIN <ADMIN@DESKTOP-6B9O9KH>
2020-06-02 15:50:56 -1200

    added branch

---
+++ helloMessagePoject/src/com/main/ChildClass.java
@@ -0,0 +1,10 @@
+package com.main;
+
+public class ChildClass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("Hello ,from child class");
+	}
+
+}
